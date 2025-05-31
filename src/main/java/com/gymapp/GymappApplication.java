124770b9de3f5d18d8373bf9338847b119bfef30
Sergio <sergio.rianxo92@gmail.com>
2025-05-31 17:19:29 +0200

    Update GymappApplication.java

--- src/main/java/com/gymapp/GymappApplication.java
+++ src/main/java/com/gymapp/GymappApplication.java
@@ -2,13 +2,14 @@ package com.gymapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class GymappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymappApplication.class, args);
-
+		System.out.println(SpringVersion.getVersion());
 	}
 
 }
