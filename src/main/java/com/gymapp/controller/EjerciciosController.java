bc408272ea5c8163d07d33f24d6db00b9e8324b6
Sergio <sergio.rianxo92@gmail.com>
2025-06-28 15:21:54 +0200

    fix pom and implement controller

--- src/main/java/com/gymapp/controller/EjerciciosController.java
+++ src/main/java/com/gymapp/controller/EjerciciosController.java
@@ -1,9 +1,33 @@
 package com.gymapp.controller;
 
 
+import com.gymapp.api.EjerciciosApi;
+import com.gymapp.model.Ejercicio;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class EjerciciosController {
+import java.util.List;
 
+@RestController
+public class EjerciciosController implements EjerciciosApi {
+
+    @Override
+    public ResponseEntity<Ejercicio> ejerciciosEjercicioIdGet(Integer ejercicioId) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> ejerciciosEjercicioIdPut(Integer ejercicioId, Ejercicio ejercicio) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<Ejercicio>> ejerciciosGet() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> ejerciciosPost(Ejercicio ejercicio) {
+        return null;
+    }
 }
