0d4406a706571670865cf4f9b313e8a8645a6ea2
Sergio <sergio.rianxo92@gmail.com>
2025-07-11 11:37:09 +0200

    test endpoint

--- src/main/java/com/gymapp/controller/EjerciciosController.java
+++ src/main/java/com/gymapp/controller/EjerciciosController.java
@@ -3,13 +3,20 @@ package com.gymapp.controller;
 
 import com.gymapp.api.EjerciciosApi;
 import com.gymapp.model.Ejercicio;
+import com.gymapp.to.mapper.EjercicioMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@AllArgsConstructor
 public class EjerciciosController implements EjerciciosApi {
+
+    private final EjercicioMapper ejercicioMapper;
 
     @Override
     public ResponseEntity<Ejercicio> ejerciciosEjercicioIdGet(Integer ejercicioId) {
@@ -23,7 +30,13 @@ public class EjerciciosController implements EjerciciosApi {
 
     @Override
     public ResponseEntity<List<Ejercicio>> ejerciciosGet() {
-        return null;
+        com.gymapp.to.domain.Ejercicio ejercicio = new com.gymapp.to.domain.Ejercicio();
+        ejercicio.setId(1);
+        ejercicio.setDescripcion("JAAJA");
+        ejercicio.setNombre("Press");
+        List<Ejercicio> ejercicios = new ArrayList<>();
+        ejercicios.add(ejercicioMapper.toTO(ejercicio));
+        return new ResponseEntity<>(ejercicios, HttpStatus.OK);
     }
 
     @Override
